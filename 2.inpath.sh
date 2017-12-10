d5db95ce8c9bd258d9e1849afe1a557e32c5428e
System Administrator <root@Hamzas-MacBook-Pro.local>
2017-12-10 22:07:46 +0800

    add chkForcmdinPath function" this command checks for the command variable in default PATH

--- 2.inpath.sh
+++ 2.inpath.sh
@@ -12,7 +12,7 @@ in_path()
 
 	for directory in "$ourpath"
 	do
-		if [ -x $directory/$cmd] then
+		if [ -x $directory/$cmd ] ; then # check if it exists
 			result = 0 # if we arrive here, command is found.
 		fi
 	done
@@ -20,3 +20,36 @@ in_path()
 	IFS=$oldIFS
 	return $result
 }
+
+chkForCmdInPath()
+{
+	# This function diffrentiates between variables that contain
+	# a single command like echo or a full given path such as
+	# /bin/echo.
+
+	var=$1
+
+	if [ "$var" != "" ] ; then
+		if [ "${var:0:1}" = "/" ] ; then # examine 1st character of variable.
+			if [ ! -x "$var" ] ; then  # check if path exists.
+				return 1
+			fi
+		elif ! in_path $var "$PATH" ; then # pass the value to in_path checking default PATH.
+			return 2
+		fi
+	fi
+}
+
+if [ $# -ne 1 ] ; then
+	echo "Usage: $0 command" >&2
+	exit 1
+fi
+
+chkForCmdInPath "$1"
+case $? in
+	0 ) echo "$1 found in PATH"					;;
+	1 ) echo "$1 not found or not executable" 	;;
+	2 ) echo "$1 not found in PATH"				;;
+esac
+
+exit 0
